5dbf0d03ca40d9850a7946c9d0bd0d62d36eabb7
Leonardo César <leo_cesa@hotmail.com>
2021-10-22 13:39:27 -0300

    feat: Create Auth redicrect

--- App.tsx
+++ App.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from "react";
 import {
   useFonts,
   Roboto_400Regular,
-  Roboto_700Bold
-} from '@expo-google-fonts/roboto';
-import AppLoading from 'expo-app-loading'
-import { StatusBar } from 'expo-status-bar';
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 
-import { Home } from './src/screens/Home';
-
+import { Home } from "./src/screens/Home";
+import { AuthProvider } from "./src/hooks/auth";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
+    Roboto_700Bold,
   });
-  
-  if(!fontsLoaded){
-    return <AppLoading />
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   return (
-    <>
-    <StatusBar style="light" />
-    <Home />
-    </>
+    <AuthProvider>
+      <StatusBar style="light"  translucent backgroundColor="transparent"  />
+      <Home />
+    </AuthProvider>
   );
 }
-
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 import { styles } from './styles'
 import { Header } from '../../components/Header'
 import { MessageList } from '../../components/MessageList'
-import { SendMessageForm } from '../../components/SendMessageForm'
+import { SignInBox } from '../../components/SignInBox'
 
 export function Home(){
     return(
@@ -12,7 +12,7 @@ export function Home(){
             <Header />
             <MessageList />
 
-            <SendMessageForm />
+            <SignInBox />
         </View>
     )
 }
